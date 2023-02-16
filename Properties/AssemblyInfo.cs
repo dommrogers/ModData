b7d6db60ee041840b88a935ff871ada8e84ad1a4
STBlade <dommrogers@gmail.com>
2023-02-16 22:25:49 +0000

    initial build, WIP

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+using MelonLoader;
+using System.Reflection;
+
+//This is a C# comment. Comments have no impact on compilation.
+
+[assembly: AssemblyTitle(BuildInfo.ModName)]
+[assembly: AssemblyCopyright($"Created by ModAuthor")]
+
+[assembly: AssemblyVersion(BuildInfo.ModVersion)]
+[assembly: AssemblyFileVersion(BuildInfo.ModVersion)]
+[assembly: MelonInfo(typeof(ModData.ModData), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.ModAuthor)]
+
+//This tells MelonLoader that the mod is only for The Long Dark.
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+
+internal static class BuildInfo
+{
+	internal const string ModName = "ModData";
+	internal const string ModAuthor = "STBlade";
+	/// <summary>
+	/// Version numbers in C# are a set of 1 to 4 positive integers separated by periods.
+	/// Mods typically use 3 numbers. For example: 1.2.1
+	/// </summary>
+	internal const string ModVersion = "1.0.0";
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # ModData
+
+**WIP - TLD utility mod for saving/loading custom mod data.**
+
+**TODO - Validation/Exceptions**
+
+* folder = \\Mods\\ModData\\\_saveSlotName_\\\_modName_
+* file ext = .moddata
+* default filename = Global
+* file format = plain text (default) or base64encoded
+
+**using ModData;**
+
+## Usage
+
+returns bool
+```cs
+ModDataManager.Save(string saveSlotName, string modName, string data);
+ModDataManager.Save(string saveSlotName, string modName, string data, bool useEncoding = false)
+ModDataManager.Save(string saveSlotName, string modName, string data, string? filename = null)
+ModDataManager.Save(string saveSlotName, string modName, string data, string? filename = null, bool useEncoding = false)
+```
+
+returns string
+```cs
+ModDataManager.Load(string saveSlotName, string modName)
+ModDataManager.Load(string saveSlotName, string modName, string? filename = null)
+```
